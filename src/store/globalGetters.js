3534257cd24df364b8f88f52165791517afe979e
Kevin Weber <mrbetis@gmail.com>
2016-11-25 23:34:41 -0500

    Fix tokenURL getter

--- src/store/globalGetters.js
+++ src/store/globalGetters.js
@@ -3,6 +3,6 @@ import sjcl from 'sjcl'
 export const tokenURL = state => {
   //   Token = base64 string => bits => unpadded base64url string
   //   WS auth token: protocol = 'Bearer+{Token}'
-  let tokenBits = sjcl.codec.base64.toBits(this.sessionToken)
+  let tokenBits = sjcl.codec.base64.toBits(state.tokens.content.token)
   return sjcl.codec.base64url.fromBits(tokenBits)
 }
